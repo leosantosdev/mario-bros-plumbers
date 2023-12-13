0c614293c1f01f85ee1c62cdded3687ed2992e26
leosantodev <dossantosleonardo.dev@gmail.com>
2023-12-13 19:24:36 -0300

    Primeiro commit - Super Mario Bros Plumbers

---
+++ script.js
@@ -0,0 +1,16 @@
+let formulario = document.querySelector(".fale-conosco")
+let mascara = document.querySelector(".mascara-form")
+
+function cliqueiNoBotao() {
+    if (formulario.styleft = "-450px") {
+
+        formulario.style.left = "calc(50% - (350px/2)"
+        mascara.style.visibility = "visible"
+    }
+
+}
+
+function sumirFormulario() {
+    formulario.style.left = "-335px"
+    mascara.style.visibility = "hidden"
+}
